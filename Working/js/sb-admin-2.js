1f77dab78d3b54d6deacf5774552987956a604a3
Kai Liu <vic.kai.liu@gmail.com>
2014-10-14 11:51:05 +0800

    WIP

--- Working/js/sb-admin-2.js
+++ Working/js/sb-admin-2.js
@@ -28,7 +28,7 @@ $(function() {
 })
 
 function generateQuestion() {
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    /* if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
     }
     else {// code for IE6, IE5
@@ -40,7 +40,7 @@ function generateQuestion() {
     xmlDoc=xmlhttp.responseXML; 
     var question=xmlDoc.getElementsByTagName("QUESTION");
     var text=question[0].getElementsByTagName("TEXT");
-    var paragraphs = text[0].getElementsByTagName("P");
+    var paragraphs = text[0].getElementsByTagName("P"); */
     
 	var textDiv = document.getElementById('text');
 	if (!textDiv) {
@@ -48,9 +48,28 @@ function generateQuestion() {
 		return;
 	}
 	
-	textDiv.innerHTML = "";
+	textDiv.innerHTML = "";	//reset content
 	for (i=0; i<paragraphs.length; i++) {
-		textDiv.innerHTML += "<p>" + paragraphs[i].childNodes[0].data + "</p>";
+		//setup
+		var regex = /([\.,-\/#!$%\^&\*;:{}=\-_`~()])/g; //regular expression for punctuations
+		var punctuations = "\.,-\/#!$%\^&\*;:{}=\-_`~()";
+		//process to word
+		var s = paragraphs[i].childNodes[0].data.replace(/(^\s*)|(\s*$)/gi,"");
+		s = s.replace(/[ ]{2,}/gi," ");
+		s = s.replace(/\n /,"\n");
+		s = s.replace(regex,' $1'); //add space before punctuations for splitting
+		words = s.split(" ");
+		//write to html
+		textDiv.innerHTML += "<p>";
+		for (j = 0; j < words.length; j++) {
+			if (punctuations.indexOf(words[j][0]) > -1) { // is a punctuation
+				textDiv.innerHTML += words[j];
+			}
+			else {
+				textDiv.innerHTML += "<button>" + words[j] + "</button>";
+			}
+		}
+		textDiv.innerHTML += "</p>"
 	}
 	
 }
